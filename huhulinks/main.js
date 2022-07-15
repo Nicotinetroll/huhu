aeb3a3190fec250c6baece7267107e4aa683e294
Nicotinetroll <dominik.majo@gmail.com>
2022-07-15 09:51:42 +0200

    Premenovane classy na huhu

--- huhulinks/main.js
+++ huhulinks/main.js
@@ -1,27 +1,30 @@
-const button = document.querySelector('.huhu_btn');
-const text = button.querySelector('.huhu_btn_text');
+
+// Parallax button
+
+const huhuButton = document.querySelector('.huhu_btn');
+const huhuText = huhuButton.querySelector('.huhu_btn_text');
 
 function parallax (e) {
-  const { offsetWidth: width, offsetHeight: height } = button;
+  const { offsetWidth: width, offsetHeight: height } = huhuButton;
   let { offsetX: x, offsetY: y } = e;
   
   const xWalk = Math.round((x / width * 100) - 50);
   const yWalk = Math.round((y / height * 100) - 50);
   
-  text.style = `
+  huhuText.style = `
     transform: translate(${(xWalk * 0.2) * +1.2}%, ${(yWalk * 0.2) * +1.2}%) scale(1.05);
   `;
-  button.style = `
+  huhuButton.style = `
     transform: translate(${(xWalk * 0.2) * +1.2}%, ${(yWalk * 0.2) * +1.2}%) scale(0.9);
   `;
 }
 
 function reset () {
-  text.style = 'transform: translate(0, 0)';
-  button.style = 'transform: translate(0, 0)';
+  huhuText.style = 'transform: translate(0, 0)';
+  huhuButton.style = 'transform: translate(0, 0)';
 }
 
 reset();
 
-button.addEventListener('mousemove', parallax);
-button.addEventListener('mouseleave', reset);
+huhuButton.addEventListener('mousemove', parallax);
+huhuButton.addEventListener('mouseleave', reset);
